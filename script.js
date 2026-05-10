f2aaf70d2444d0c27ba6e4713a2c960b271fdd45
rymasalghamdy32-oss <rymasalghamdy32@gmail.com>
2026-05-10 14:15:44 +0300

    Final Website upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function showoffer(){
+
+    alert("20% OFF on all skincare treatments today!");
+}
+/* CONTACT PAGE */
+const contactForm = document.getElementById("contactForm");
+const confirmMessage = document.getElementById("confirmMessage");
+
+contactForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    confirmMessage.style.display = "block";
+
+    setTimeout(() => {
+        confirmMessage.style.opacity = "1";
+    }, 100);
+
+    contactForm.reset();
+});
+setTimeout(() => {
+    confirmMessage.style.opacity = "0";
+    setTimeout(() => {
+        confirmMessage.style.display = "none";
+    }, 300);
+}, 3000);
+if (!contactForm.checkValidity()) {
+    alert("Please fill all fields correctly.");
+    return;
+}
+const button = contactForm.querySelector("button");
+
+button.textContent = "Sending...";
+button.disabled = true;
+
+setTimeout(() => {
+    button.textContent = "Send Message";
+    button.disabled = false;
+}, 1500);
+// Booking Page
+function selectService(el) {
+    document.querySelectorAll('.service-option').forEach(opt => {
+        opt.classList.remove('selected');
+    });
+    el.classList.add('selected');
+}
+
+function confirmBooking() {
+    const msg = document.getElementById('confirmMsg');
+    msg.style.display = 'block';
+    setTimeout(() => {
+        msg.style.opacity = '1';
+    }, 10);
+}
